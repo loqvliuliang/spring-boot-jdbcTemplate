2c9bc831ecb0b86fdd995d44034ce995d64062ff
刘亮 <liang.liu04@hand-china.com>
2017-08-02 20:49:02 +0800

    提交

---
+++ src/main/java/com/example/demo/Service/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.demo.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ll on 2017/8/1.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public void create(String name, Integer age) {
+        jdbcTemplate.update("insert into USER(NAME, AGE) values(?, ?)", name, age);
+    }
+    @Override
+    public void deleteByName(String name) {
+        jdbcTemplate.update("delete from USER where NAME = ?", name);
+    }
+    @Override
+    public Integer getAllUsers() {
+        return jdbcTemplate.queryForObject("select count(1) from USER", Integer.class);
+    }
+    @Override
+    public void deleteAllUsers() {
+        jdbcTemplate.update("delete from USER");
+    }
+}
---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+/**
+ * Created by ll on 2017/8/1.
+ */
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+}
